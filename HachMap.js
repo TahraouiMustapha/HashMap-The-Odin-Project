1c978260664ee1c139433b11ea97c164df30fb07
TahraouiMustapha <moustaphaoussama1818@gmail.com>
2024-12-28 20:07:19 +0100

    feat : create hash , set methods (with logic that check whether the key exist or not)

--- HachMap.js
+++ HachMap.js
@@ -1,10 +1,46 @@
 class HashMap {
+    buckets = new Array(16);
+    
     constructor() {
         this.loadFactor = 0.75;
-        this.capacity = 16;
+        this.capacity = this.buckets.length;
+    }
+
+    hash(key) {
+        let hashCode = 0;
+           
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+          hashCode = primeNumber * hashCode + key.charCodeAt(i);
+          hashCode %= this.capacity;
+        }
+     
+        return hashCode;
+    } 
+
+    set(key, value) {
+        const index = this.hash(key);
+        if(!this.buckets[index]) {
+            this.buckets[index] = { key , value };
+        } else {
+            if(this.buckets[index].key == key) {
+                // the key is already exist!
+                this.buckets[index].value = value ;
+            } else {
+                // collision
+            }
+        }
     }
 
 }
 
-const n = new HashMap();
-console.log(n.capacity)
+
+const myHash = new HashMap();
+console.log(myHash.set('john', 1));
+console.log(myHash.set('john', 2));
+console.log(myHash.set('john', 2));
+// console.log(myHash.set('john', 1));
+
+
+
+
